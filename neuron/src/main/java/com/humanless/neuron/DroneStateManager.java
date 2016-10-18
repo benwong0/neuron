67ccc7caeac1a612096621cd629580ea31a6b90c
Ben Wong <ben@hivemapper.com>
2016-10-17 23:44:59 -0700

    Add ability to only update state when value is changed

--- neuron/src/main/java/com/humanless/neuron/DroneStateManager.java
+++ neuron/src/main/java/com/humanless/neuron/DroneStateManager.java
@@ -2,28 +2,55 @@ package com.humanless.neuron;
 
 import android.util.SparseArray;
 
+import java.util.Date;
+
 /**
  * Base drone state manager. Drone state manager is responsible for maintaining the current state
  * of the drone.
  */
 public abstract class DroneStateManager {
     private SparseArray<Object> states = new SparseArray<>();
+    private SparseArray<Date> updateTimes = new SparseArray<>();
 
     /**
-     * Set the value of the specified state.
+     * Set the value of the specified state. State will not be set if the value is the same as
+     * existing value.
      *
      * @param state The state to set.
      * @param value The value.
+     * @return true, if state is updated; otherwise, false.
      */
-    public void setState(int state, Object value) {
-        states.setValueAt(state, value);
+    public boolean setState(int state, Object value) {
+        return setStates(state, value, false);
+    }
+
+    /**
+     * Set the value of the specified state. State will not be set if the value is the same as
+     * existing value, unless force is true.
+     *
+     * @param state The state to set.
+     * @param value The value.
+     * @param force If true, the value will be set reguardless of existing value; otherwise, it will
+     *              only set if the value is different from existing value.
+     * @return true, if state is updated; otherwise, false.
+     */
+    public boolean setStates(int state, Object value, boolean force) {
+        Object currentValue = getState(state);
+
+        if (currentValue != value || force) {
+            states.setValueAt(state, value);
+            updateTimes.setValueAt(state, new Date());
+            return true;
+        }
+
+        return false;
     }
 
     /**
      * Get the current value of the specified state.
      *
      * @param state The state to retrieve.
-     * @return The value.
+     * @return The existing value of the state. Value can be null.
      */
     public Object getState(int state) {
         return states.get(state);
